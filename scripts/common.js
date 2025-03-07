c9836ac94ba0844f2585940f2daf154caf649331
nickem00 <nickemalm00@gmail.com>
2025-03-07 14:39:14 +0100

    Animations and information section on index

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Elements
+    const header = document.querySelector("header div");
+    const hero = document.querySelector(".hero");
+    const contactForm = document.querySelector(".contact-form-container")
+    const informationSection = document.getElementById("information-section")
+
+    if (header) {
+        setTimeout(() => {
+            header.classList.add("header-loaded")
+        }, 50)
+    }
+
+    if (hero) {
+        setTimeout(() => {
+            hero.classList.add("fade-fall")
+        }, 50)
+    }
+
+    if (contactForm) {
+        setTimeout(() => {
+            contactForm.classList.add("fade-fall")
+        }, 50)
+    }
+
+    if (informationSection) {
+        setTimeout(() => {
+            informationSection.classList.add("fade-fall")
+        }, 50)
+    }
+    
+    
+    
     const currentPage = window.location.pathname.split("/").pop();
     document.querySelectorAll(".nav-links a").forEach(link => {
         if (link.getAttribute("href") === currentPage) {
